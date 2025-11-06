dc178fd19f24fa111800b71617f2ca159ad7c67e
anishsinghQB <anish.singh@quadbtech.com>
2025-11-06 11:30:22 +0530

    fix backend : remove the unused script files

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser'
-import morgan from 'morgan';
 import helmet from 'helmet';
 import connectDB from './config/connectDb.js';
 import userRouter from './route/user.route.js'
@@ -31,14 +30,12 @@ app.use(cors({
 
 app.use(express.json())
 app.use(cookieParser())
-// app.use(morgan())
 app.use(helmet({
     crossOriginResourcePolicy: false
 }))
 
 
 app.get("/", (request, response) => {
-    ///server to client
     response.json({
         message: "Server is running " + process.env.PORT
     })
