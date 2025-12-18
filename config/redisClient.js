93d61a4f02788f988957a865b9c4da449aff57b4
anish-singh-rawat <www.computernetowrk@gmail.com>
2025-12-18 14:25:26 +0530

    fix backend : correct code for connect backend servers

--- config/redisClient.js
+++ config/redisClient.js
@@ -3,21 +3,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-// Configure Redis client for local development
-// export const redis = new Redis({
-//   host: process.env.REDIS_HOST || '127.0.0.1',
-//   port: process.env.REDIS_PORT || 6379,
-//   password: process.env.REDIS_PASSWORD || undefined,
-// });
+export const redis = new Redis({
+  host: '127.0.0.1',
+  port: 6379,
+});
 
 // For production use TLS
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  tls: {} 
-});
+// export const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   username: process.env.REDIS_USERNAME,
+//   password: process.env.REDIS_PASSWORD,
+//   tls: {} 
+// });
 
 try {
   redis.on('connect', () => {
